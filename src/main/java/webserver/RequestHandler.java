62879ea47a8e45a36a2b1202e17e4d9a53d33d1c
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-04 01:25:39 +0900

    요구사항 1 리팩토링
    생각한거
    - 함수 하나가 하나의 역할
    - 함수 줄 수 줄이기
    - 하드코딩 피하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,6 +10,8 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
+    private static final String DIRECTORY = "./webapp";
+
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -21,10 +23,12 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            String requestUrl = readInputByLine(in);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String requestUrl = readRequestLine(br);
+            printHeaders(br, requestUrl);
 
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
+            byte[] body = Files.readAllBytes(new File(DIRECTORY + requestUrl).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -32,23 +36,23 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String readInputByLine(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    private String readRequestLine(BufferedReader br) throws IOException {
         String line = br.readLine();
         log.info("request line : {}", line);
         if (line == null) {
-            return line;
+            throw new RuntimeException("connection exception happen");
         }
-
         String requestUrl = line.split(" ")[1];
+        String index = "/index.html";
+        return requestUrl.equals("/") ? index : requestUrl;
+    }
 
+    private void printHeaders(BufferedReader br, String line) throws IOException {
         while (!line.equals("")) {
             line = br.readLine();
             log.info("header : {}", line);
-            if (line == null) return line;
+            if (line == null) break;
         }
-
-        return requestUrl.equals("/") ? "/index.html" : requestUrl;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
