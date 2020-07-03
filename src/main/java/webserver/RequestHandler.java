de7e6045dcb8bc7e39225cc709a0ca6c7517e82d
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-04 01:03:50 +0900

    요구사항 1 구현
    1. 웹 서버 요청 헤더 로그 찍기
    2. 요청 받은 url과 매칭되는 파일 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +21,34 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String requestUrl = readInputByLine(in);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String readInputByLine(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+        log.info("request line : {}", line);
+        if (line == null) {
+            return line;
+        }
+
+        String requestUrl = line.split(" ")[1];
+
+        while (!line.equals("")) {
+            line = br.readLine();
+            log.info("header : {}", line);
+            if (line == null) return line;
+        }
+
+        return requestUrl.equals("/") ? "/index.html" : requestUrl;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
